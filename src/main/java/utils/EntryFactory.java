e6b07d5564aefbd11d4709566cc4cf550e9410ac
Petr Markov <channelpitnik@gmail.com>
2020-11-16 03:02:10 +0300

    трехчасовоночный понедельничный коммит
    лаба как была рабочей так и остается
    это радует
    добавил p:remoteCommand, которая позволяет js вызывать форму p:commandButton
    оч удобно
    убрал лишнее из EntryFactory, сборщик мусора от костылей ace:sliderEntry так сказать
    поправил чутка css
    ну и validator js постепенно нарастает фишками, уже красивенько рисует и может посылать запросы при клике на канвас
    гип гип ура

--- src/main/java/utils/EntryFactory.java
+++ src/main/java/utils/EntryFactory.java
@@ -18,15 +18,4 @@ public class EntryFactory {
         res.setStatus(bean.getStatus());
         return res;
     }
-    
-    public Double getRealValue(Double wrongValue) {
-        if (wrongValue == 1) return 1.0;
-        else if (wrongValue == 2) return 1.5;
-        else if (wrongValue == 3) return 2.0;
-        else if (wrongValue == 4) return 2.5;
-        else if (wrongValue == 5) return 3.0;
-        else if (wrongValue == 6) return 3.5;
-        else if (wrongValue == 7) return 4.0;
-        else return 1.0;
-    }
 }
--- src/main/webapp/resources/js/validator.js
+++ src/main/webapp/resources/js/validator.js
@@ -1,6 +1,6 @@
 let x = 1;
 let y;
-let z = 1;
+let r = 1;
 
 function pressX(id){
     (x === parseInt(id.trim().substr(8))) ? document.getElementById(id).checked = true : x = parseInt(id.trim().substr(8));
@@ -8,10 +8,7 @@ function pressX(id){
     console.log('X = ' + x);
     document.getElementById('form:x').value = x;
 }
-function checkR(){
-    let check = false;
-    for(let i=-3 ;i<=5; i++) if (document.getElementById('form:bcb'+i).checked === true) check = true;
-}
+
 function getY(){
     y =  document.getElementById('form:y').value;
 }
@@ -20,3 +17,55 @@ function getR(){
     r =  document.getElementById('form:r').value;
 }
 
+function draw() {
+    let entries = getEntries();
+    if (entries === null) entries = [];
+    entries.forEach(e => drawPoint(e.x,e.y,e.r,e.status));
+}
+
+
+function getEntries() {
+    let res = [];
+    let entries = document.getElementById("entries_table_data").childNodes;
+    for (let i = 0; i < entries.length; i++) {
+        let values = entries[i].childNodes;
+        if (values.length === 1) {
+            continue;
+        }
+        res.push({
+            x: values[1].innerText,
+            y: values[2].innerText,
+            r: values[3].innerText,
+            result: values[4].innerText
+        })
+    }
+    return res;
+}
+
+function show_coords(event) {
+        let rect = event.currentTarget.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        let basis = 100 / $('#form\\:r').val();
+        let cx = ((x - 150) / basis);
+        let cy = ((150 - y) / basis);
+        console.log("X coords: " + x + ", Y coords: " + y + ",R: " + basis);
+        console.log("X coords: " + cx + ", Y coords: " + cy);
+        $('#form\\:x').val(cx);
+        $('#form\\:y').val(cy);
+        //drawPoint(cx, cy, $('#form\\:r').val(), true ) // ВРЕМЕННО
+        send();
+}
+
+function drawPoint(x_value, y_value, r, flag) {
+    let canvas = document.getElementById('canvas');
+    let context = canvas.getContext('2d');
+    let x = x_value * 100 / r + 150;
+    let y = 150 - y_value * 100 / r;
+    if (flag) context.fillStyle = "green";
+    if (!flag) context.fillStyle = "red";
+    context.beginPath();
+    context.arc(x, y, 2, 0, Math.PI * 2, false);
+    context.fill();
+    context.stroke();
+}
